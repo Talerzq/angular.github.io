1d0686c2646d676b488d2bf5795370def82c4905
Talerzq <antonikaczorowski26@gmail.com>
2025-11-12 09:40:04 +0100

    Deploy Angular app to GitHub Pages

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/angular',
+  basePath: '/Zadania',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/angular/',
+  baseHref: '/Zadania/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/angular"
+    "route": "/Zadania"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 440, hash: '9ccd17c71eeadec9696027887eaacdcd22ab318e03b673a6ab208c665b744d6e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 953, hash: '0e3734d7e5fcb851ffa8182eb0c1d335a092b09827f2ef7833aa0ac974eda853', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 3503, hash: '18522d43a1955b6b5542b1bdb86a3c932a4d5f414053d3857ef96149bc745aeb', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 440, hash: 'de01c074e181e21624057ffa6c0f4fc4527acf3f80c201717ac822ccccbb01b3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 953, hash: '1e85ff52d2ea730384ed892f9b4f0088c7be65c1e12dd122ba4ba67e5dad5b96', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 3503, hash: '2ecdfa0c2422fdb524ed99a7ad09f5013e13c6cd1ea8ebd4cee413961e7d212c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
